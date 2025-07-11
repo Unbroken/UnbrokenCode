db4c5a26ddb529a9ebf2f308f4ebb248dae0e15c
Erik Olofsson <erik@olofsson.info>
2025-07-11 13:03:46 +0200

    Add support for changing color spaces for theme colors
    [no release notes]

--- src/vs/editor/standalone/browser/standaloneCodeEditor.ts
+++ src/vs/editor/standalone/browser/standaloneCodeEditor.ts
@@ -16,7 +16,7 @@ import { StandaloneKeybindingService, updateConfigurationService } from './stand
 import { IStandaloneThemeService } from '../common/standaloneTheme.js';
 import { IMenuItem, MenuId, MenuRegistry } from '../../../platform/actions/common/actions.js';
 import { CommandsRegistry, ICommandHandler, ICommandService } from '../../../platform/commands/common/commands.js';
-import { IConfigurationService } from '../../../platform/configuration/common/configuration.js';
+import { IConfigurationService, IConfigurationChangeEvent } from '../../../platform/configuration/common/configuration.js';
 import { ContextKeyExpr, ContextKeyValue, IContextKey, IContextKeyService } from '../../../platform/contextkey/common/contextkey.js';
 import { IContextMenuService } from '../../../platform/contextview/browser/contextView.js';
 import { IInstantiationService, ServicesAccessor } from '../../../platform/instantiation/common/instantiation.js';
@@ -44,6 +44,7 @@ import { IHoverService, WorkbenchHoverDelegate } from '../../../platform/hover/b
 import { setBaseLayerHoverDelegate } from '../../../base/browser/ui/hover/hoverDelegate2.js';
 import { IMarkdownRendererService } from '../../../platform/markdown/browser/markdownRenderer.js';
 import { EditorMarkdownCodeBlockRenderer } from '../../browser/widget/markdownRenderer/browser/editorMarkdownCodeBlockRenderer.js';
+import { RGBColorSpace } from '../../../base/common/color.js';
 
 /**
  * Description of an action contribution
@@ -451,6 +452,13 @@ export class StandaloneEditor extends StandaloneCodeEditor implements IStandalon
 		this._standaloneThemeService = themeService;
 		this._register(themeDomRegistration);
 
+		themeService.setHighlightingColorSpace(this.getHighlightingColorSpace());
+		this._register(configurationService.onDidChangeConfiguration((event: IConfigurationChangeEvent) => {
+			if (event.affectsConfiguration('workbench.highlightingColorSpace')) {
+				themeService.setHighlightingColorSpace(this.getHighlightingColorSpace());
+			}
+		}));
+
 		let model: ITextModel | null;
 		if (typeof _model === 'undefined') {
 			const languageId = languageService.getLanguageIdByMimeType(options.language) || options.language || PLAINTEXT_LANGUAGE_ID;
@@ -469,6 +477,15 @@ export class StandaloneEditor extends StandaloneCodeEditor implements IStandalon
 			};
 			this._onDidChangeModel.fire(e);
 		}
+	}
+
+	private getHighlightingColorSpace(): RGBColorSpace {
+		const space = this._configurationService.getValue<RGBColorSpace | 'default'>('workbench.highlightingColorSpace');
+		if (space !== 'default') {
+			return space;
+		}
+
+		return null;
 	}
 
 	public override dispose(): void {
@@ -539,6 +556,22 @@ export class StandaloneDiffEditor2 extends DiffEditorWidget implements IStandalo
 		this._standaloneThemeService = themeService;
 
 		this._register(themeDomRegistration);
+
+		themeService.setHighlightingColorSpace(this.getHighlightingColorSpace());
+		this._register(configurationService.onDidChangeConfiguration((event: IConfigurationChangeEvent) => {
+			if (event.affectsConfiguration('workbench.highlightingColorSpace')) {
+				themeService.setHighlightingColorSpace(this.getHighlightingColorSpace());
+			}
+		}));
+	}
+
+	private getHighlightingColorSpace(): RGBColorSpace {
+		const space = this._configurationService.getValue<RGBColorSpace | 'default'>('workbench.highlightingColorSpace');
+		if (space !== 'default') {
+			return space;
+		}
+
+		return null;
 	}
 
 	public override dispose(): void {

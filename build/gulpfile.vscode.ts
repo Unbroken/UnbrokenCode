0bcf6eeccb8988fe457c6875de81b001c100a14c
Erik Olofsson <erik@olofsson.info>
2025-08-14 21:12:30 +0200

    Default to 'code' command to be compatible with tools such as Claude Code

--- build/gulpfile.vscode.ts
+++ build/gulpfile.vscode.ts
@@ -397,7 +397,7 @@ function packageTask(platform: string, arch: string, sourceFolderName: string, d
 				result = es.merge(result, gulp.src('resources/win32/insider/bin/code.cmd', { base: 'resources/win32/insider' })
 					.pipe(replace('@@NAME@@', product.nameShort))
 					.pipe(replace('@@VERSIONFOLDER@@', versionedResourcesFolder))
-					.pipe(rename(function (f) { f.basename = product.applicationName; })));
+					.pipe(rename(function (f) { f.basename = 'code'; })));
 
 				result = es.merge(result, gulp.src('resources/win32/insider/bin/code.sh', { base: 'resources/win32/insider' })
 					.pipe(replace('@@NAME@@', product.nameShort))
@@ -408,11 +408,11 @@ function packageTask(platform: string, arch: string, sourceFolderName: string, d
 					.pipe(replace('@@VERSIONFOLDER@@', versionedResourcesFolder))
 					.pipe(replace('@@SERVERDATAFOLDER@@', product.serverDataFolderName || '.vscode-remote'))
 					.pipe(replace('@@QUALITY@@', quality))
-					.pipe(rename(function (f) { f.basename = product.applicationName; f.extname = ''; })));
+					.pipe(rename(function (f) { f.basename = 'code'; f.extname = ''; })));
 			} else {
 				result = es.merge(result, gulp.src('resources/win32/bin/code.cmd', { base: 'resources/win32' })
 					.pipe(replace('@@NAME@@', product.nameShort))
-					.pipe(rename(function (f) { f.basename = product.applicationName; })));
+					.pipe(rename(function (f) { f.basename = 'code'; })));
 
 				result = es.merge(result, gulp.src('resources/win32/bin/code.sh', { base: 'resources/win32' })
 					.pipe(replace('@@NAME@@', product.nameShort))
@@ -422,7 +422,7 @@ function packageTask(platform: string, arch: string, sourceFolderName: string, d
 					.pipe(replace('@@APPNAME@@', product.applicationName))
 					.pipe(replace('@@SERVERDATAFOLDER@@', product.serverDataFolderName || '.vscode-remote'))
 					.pipe(replace('@@QUALITY@@', String(quality)))
-					.pipe(rename(function (f) { f.basename = product.applicationName; f.extname = ''; })));
+					.pipe(rename(function (f) { f.basename = 'code'; f.extname = ''; })));
 			}
 
 			result = es.merge(result, gulp.src('resources/win32/VisualElementsManifest.xml', { base: 'resources/win32' })
--- src/vs/workbench/electron-browser/actions/installActions.ts
+++ src/vs/workbench/electron-browser/actions/installActions.ts
@@ -6,12 +6,10 @@
 import { localize, localize2 } from '../../../nls.js';
 import { Action2 } from '../../../platform/actions/common/actions.js';
 import { ILocalizedString } from '../../../platform/action/common/action.js';
-import product from '../../../platform/product/common/product.js';
 import { IDialogService } from '../../../platform/dialogs/common/dialogs.js';
 import { ServicesAccessor } from '../../../platform/instantiation/common/instantiation.js';
 import { INativeHostService } from '../../../platform/native/common/native.js';
 import { toErrorMessage } from '../../../base/common/errorMessage.js';
-import { IProductService } from '../../../platform/product/common/productService.js';
 import { isCancellationError } from '../../../base/common/errors.js';
 
 const shellCommandCategory: ILocalizedString = localize2('shellCommand', 'Shell Command');
@@ -21,7 +19,7 @@ export class InstallShellScriptAction extends Action2 {
 	constructor() {
 		super({
 			id: 'workbench.action.installCommandLine',
-			title: localize2('install', "Install '{0}' command in PATH", product.applicationName),
+			title: localize2('install', "Install 'code' command in PATH"),
 			category: shellCommandCategory,
 			f1: true
 		});
@@ -30,12 +28,11 @@ export class InstallShellScriptAction extends Action2 {
 	async run(accessor: ServicesAccessor): Promise<void> {
 		const nativeHostService = accessor.get(INativeHostService);
 		const dialogService = accessor.get(IDialogService);
-		const productService = accessor.get(IProductService);
 
 		try {
 			await nativeHostService.installShellCommand();
 
-			dialogService.info(localize('successIn', "Shell command '{0}' successfully installed in PATH.", productService.applicationName));
+			dialogService.info(localize('successIn', "Shell command 'code' successfully installed in PATH."));
 		} catch (error) {
 			if (isCancellationError(error)) {
 				return;
@@ -51,7 +48,7 @@ export class UninstallShellScriptAction extends Action2 {
 	constructor() {
 		super({
 			id: 'workbench.action.uninstallCommandLine',
-			title: localize2('uninstall', "Uninstall '{0}' command from PATH", product.applicationName),
+			title: localize2('uninstall', "Uninstall 'code' command from PATH"),
 			category: shellCommandCategory,
 			f1: true
 		});
@@ -60,12 +57,11 @@ export class UninstallShellScriptAction extends Action2 {
 	async run(accessor: ServicesAccessor): Promise<void> {
 		const nativeHostService = accessor.get(INativeHostService);
 		const dialogService = accessor.get(IDialogService);
-		const productService = accessor.get(IProductService);
 
 		try {
 			await nativeHostService.uninstallShellCommand();
 
-			dialogService.info(localize('successFrom', "Shell command '{0}' successfully uninstalled from PATH.", productService.applicationName));
+			dialogService.info(localize('successFrom', "Shell command 'code' successfully uninstalled from PATH."));
 		} catch (error) {
 			if (isCancellationError(error)) {
 				return;

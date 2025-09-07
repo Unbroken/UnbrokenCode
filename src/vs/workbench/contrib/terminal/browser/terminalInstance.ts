73597890e8ee4d9bbdefe693a2156ea476d24437
Erik Olofsson <erik@olofsson.info>
2025-09-07 11:42:15 +0200

    Make parsing terminal line output reliable for problem matchers on Windows

--- src/vs/workbench/contrib/terminal/browser/terminalInstance.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalInstance.ts
@@ -1484,6 +1484,7 @@ export class TerminalInstance extends Disposable implements ITerminalInstance {
 	clearBuffer(): void {
 		this._processManager.clearBuffer();
 		this.xterm?.clearBuffer();
+		this._lineDataEventAddon?.reset();
 	}
 
 	private _refreshSelectionContextKey() {

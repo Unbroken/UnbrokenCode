b99f7b825d2c8850a0b8afa152ba35c641f2fe56
Erik Olofsson <erik@olofsson.info>
2026-01-15 01:10:22 +0100

    Fix auto updates on Windows

--- src/vs/platform/update/electron-main/updateService.win32.ts
+++ src/vs/platform/update/electron-main/updateService.win32.ts
@@ -126,8 +126,9 @@ export class Win32UpdateService extends AbstractUpdateService implements IRelaun
 	protected override async initialize(): Promise<void> {
 		if (this.productService.win32VersionedUpdate) {
 			const cachePath = await this.cachePath;
-			app.setPath('appUpdate', cachePath);
-			await this.unlink(path.join(cachePath, 'session-ending.flag'));
+			try {
+				await this.unlink(path.join(cachePath, 'session-ending.flag'));
+			} catch { }
 		}
 
 		// Send telemetry

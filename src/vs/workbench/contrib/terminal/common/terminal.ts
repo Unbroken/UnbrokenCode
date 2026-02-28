fab74b2dc3bcb05a49d375bd85f7d9faa7fb5406
Erik Olofsson <erik@olofsson.info>
2026-02-28 14:23:39 +0100

    Add support for flashing and desktop notifications for terminal bells

--- src/vs/workbench/contrib/terminal/common/terminal.ts
+++ src/vs/workbench/contrib/terminal/common/terminal.ts
@@ -174,6 +174,8 @@ export interface ITerminalConfiguration {
 	confirmOnExit: ConfirmOnExit;
 	confirmOnKill: ConfirmOnKill;
 	enableBell: boolean;
+	enableFlashBell: boolean;
+	enableBellNotification: boolean;
 	env: {
 		linux: { [key: string]: string };
 		osx: { [key: string]: string };
